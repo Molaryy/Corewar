ec3bdcda82341f753aebad06a2ce3a519c2bc5e3
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-05-09 04:12:20 +0200

    feat: modified name champions to asm -> (includes/asm.h)-

---
+++ includes/asm.h
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2023
+** corewar
+** File description:
+** champion
+*/
+
+#ifndef asm
+    #define asm
+    #include <stdio.h>
+
+    typedef struct header_s {
+
+        char *name;
+        char *description;
+
+    } header_t;
+
+    typedef struct body_s {
+
+        char label_char;
+        char *hexa;
+        char *name;
+        size_t nb_param;
+        bool reg;
+        bool direct;
+        bool indirect;
+
+    } body_t;
+
+/**
+ * @brief detects if the file has the correct extension
+ *
+ * @param filepath
+ * @return true
+ * @return false
+ */
+bool detect_file_extension(char const *filepath);
+#endif /* !asm */
