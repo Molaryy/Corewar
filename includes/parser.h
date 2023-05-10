eab3238c53734bcf0705520ab0aae00afc461044
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-05-10 15:30:41 +0200

    feat: add parser include to store all the parsing functions -> (includes/parser.h)-

---
+++ includes/parser.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2023
+** PROJECT
+** File description:
+** parser.h
+*/
+
+#ifndef PARSER_H_
+    #define PARSER_H_
+
+    typedef struct parser_s {
+
+        char *name;
+        size_t p3;
+        size_t nb_params;
+        size_t p1;
+        size_t p2;
+
+    } parser_t;
+#endif /*PARSER_H_*/
