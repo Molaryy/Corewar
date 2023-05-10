93aeb9a4f430c57dcfb26a23e2657fef11000c64
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-05-10 16:38:14 +0200

    feat: add getFile info -> (asm/parsing/parse_reference_file.c)-

--- asm/parsing/parse_reference_file.c
+++ asm/parsing/parse_reference_file.c
@@ -9,23 +9,36 @@
 #include "asm.h"
 #include "parser.h"
 
-static char **get_file(char const *filepath)
+static char **get_file(char *filepath)
 {
-    int FD = open(filepath, O_RDONLY);
-    char *buffer = NULLL;
+    int fd = open(filepath, O_RDONLY);
+    char *buffer = NULL;
     char **fileTab = NULL;
     struct stat info;
 
     if (stat(filepath, &info) == -1)
-        return false;
+        return NULL;
     buffer = malloc(sizeof(char) * (info.st_size + 1));
     if (!buffer)
-        return false;
+        return NULL;
     buffer[info.st_size] = '\0';
-    if (read(fd, filepath, info.st_size) == -1)
-        return false;
+    if (read(fd, buffer, info.st_size) == -1)
+        return NULL;
     fileTab = str_to_array_separator(buffer, "\n");
     free(buffer);
-    close(FD);
+    close(fd);
     return fileTab;
+}
+
+parser_t *init_parser_reference(char *filepath)
+{
+    char **parsTab = get_file(filepath);
+    parser_t *parser = NULL;
+    size_t len = count_tab(parsTab);
+
+    if (!parsTab || len != NB_INSTRIUCTIONS)
+        return NULL;
+    parser = malloc(sizeof(parser_t) *(len));
+    my_printf("len = %d", len);
+    return parser;
 }
