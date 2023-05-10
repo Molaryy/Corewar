21ddbfedadd37b4334c63d97fc267edf2dcf1c8f
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-05-10 15:53:33 +0200

    feat: added str to array with separators functions -> (lib/jb/src/features/array/str_to_array_separator.c)-

---
+++ lib/jb/src/features/array/str_to_array_separator.c
@@ -0,0 +1,64 @@
+/*
+** EPITECH PROJECT, 2022
+** my_str_to_word_array
+** File description:
+** A function that splits into words
+*/
+
+#include "jb.h"
+
+static bool is_separator(char c, char *separator)
+{
+    for (size_t i = 0; separator[i]; i++) {
+        if (separator[i] == c)
+            return false;
+    }
+    return true;
+}
+
+static size_t my_cols_counter(char const *str, size_t i, char *separator)
+{
+    size_t word = 0;
+
+    for (; str[i] != '\0' && !is_separator(str[i], separator); i++);
+    for (; str[i] != '\0' && is_separator(str[i], separator); i++)
+        word++;
+    return word;
+}
+
+static size_t count_lines(char const *str, char *separator)
+{
+    size_t n = 0;
+
+    for (size_t i = 0; str[i] != '\0'; i++) {
+        if ((is_separator(str[i], separator))
+        && (!is_separator(str[i + 1], separator)
+        || str[i + 1] == '\0')) {
+            n++;
+        }
+    }
+    return (n);
+}
+
+char **str_to_array_separator(char const *str, char *separator)
+{
+    size_t lines = count_lines(str, separator);
+    char **array = malloc(sizeof(char *) * (lines + 1));
+
+    if (!array)
+        return NULL;
+    for (size_t i = 0, k = 0, j = 0, cols = 0; i < lines; i++) {
+        cols = my_cols_counter(str, j, separator);
+        array[i] = malloc(sizeof(char) * (cols + 1));
+        if (!array[i])
+            return NULL;
+        array[i][cols] = '\0';
+        for (; str[j] != '\0' && (!is_separator(str[j], separator)); j++);
+        for (; str[j] != '\0' && is_separator(str[j], separator); j++, k++) {
+            array[i][k] = str[j];
+        }
+        k = 0;
+    }
+    array[lines] = NULL;
+    return (array);
+}
