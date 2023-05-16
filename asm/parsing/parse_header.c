68b74f296b3ddf02306f669893c0e20b5f86a901
Bollore <quentin.bollore@epitech.eu>
2023-05-16 21:44:03 +0200

    feat: parsing of the header -> (asm/parsing/parse_header.c)-

--- asm/parsing/parse_header.c
+++ asm/parsing/parse_header.c
@@ -8,58 +8,43 @@
 #include "asm.h"
 #include "jb.h"
 
-bool is_commentary(char *line)
-{
-    if (!line)
-        return false;
-    if (line[0] == COMMENT_CHAR)
-        return true;
-    else
-        return false;
-}
-
-bool is_name(char *line)
-{
-    if (!line)
-        return false;
-    if (my_strcmp(line, NAME_CMD_STRING))
-        return true;
-    else 
-        return false;
-}
-
-bool is_description(char *line)
-{
-    if (!line)
-        return false;
-    if (my_strcmp(line, COMMENT_CMD_STRING))
-        return true;
-    else 
-        return false;
-}
-
 void clean_array(char **array)
 {
     for (int i = 0; array[i]; i++)
         clean_string(array[i]);
 }
 
-void parse_header(file_t *file, char *filepath) 
+int check_header(char **parser, file_t *file, size_t status)
+{
+    clean_array(parser);
+    if (is_name(parser[0]))
+        file->header->name = my_strdup(parser[1]);
+    if (is_description(parser[0]))
+        file->header->description = my_strdup(parser[1]);
+    if (!check_nb_arg(parser, 2))
+        status = FAILURE;
+    return status;
+}
+
+int parse_header(file_t *file, char *filepath)
 {
     char **parser = NULL;
+    size_t status = 0;
 
     file->origin_file = get_file(filepath);
     clean_array(file->origin_file);
-
     for (int i = 0; file->origin_file[i]; i++) {
         if (file->origin_file[i][0] != '.' && file->origin_file[i][0] != '#')
             break;
         if (!is_commentary(file->origin_file[i])) {
             parser = str_to_array_separator(file->origin_file[i], "\"");
-            my_printf("%t", parser);
-            //printf("%s\n", file->origin_file[i][0]);
+            status = check_header(parser, file, status);
+            free_array_str(parser);
         }
+        if (status == FAILURE)
+            return status;
     }
-    clean_array(parser);
-    free_array_str(parser);
+    if (!file->header->name || !file->header->description)
+        status = FAILURE;
+    return status;
 }
