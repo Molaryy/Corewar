36dd34601831f654abb097b3e1273a9b212e11c5
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-05-18 01:44:39 +0200

    feat: add check extension files tests -> (tests/tests.c)-

--- tests/tests.c
+++ tests/tests.c
@@ -8,4 +8,17 @@
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
 #include "jb.h"
+#include "asm.h"
+#include "parser.h"
 
+Test(test_extension_1, test_extension_nb_1) {
+    char const *filepath = "file.txt";
+    bool result = detect_file_extension(filepath);
+    cr_assert_eq(false, result);
+}
+
+Test(test_extension_2, test_extension_nb_1) {
+    char const *filepath = "file.s";
+    bool result = detect_file_extension(filepath);
+    cr_assert_eq(true, result);
+}
