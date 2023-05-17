3cccf6c559b8565d05d6ca16cc1f7871e21a2139
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-05-18 01:56:33 +0200

    feat: started func to check the types in the arguments -> (asm/parsing/body/check_type_arguments.c)-

---
+++ asm/parsing/body/check_type_arguments.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2023
+** PROJECT
+** File description:
+** check_type_arguments
+*/
+
+#include "jb.h"
+#include "asm.h"
+#include "parser.h"
+
+bool check_type_arguments(body_t *body, parser_t *pars)
+{
+    (void) body;
+    (void) pars;
+
+    return true;
+}
