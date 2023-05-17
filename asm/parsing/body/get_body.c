89fcb0dec0a44313c5dc53e10133f74e80f8675a
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-05-17 23:58:55 +0200

    feat: change name body array -> (asm/parsing/body/get_body.c)-

--- asm/parsing/body/get_body.c
+++ asm/parsing/body/get_body.c
@@ -8,17 +8,42 @@
 #include "asm.h"
 #include "jb.h"
 
-static char **get_body(char **fileTab)
+static size_t count_lines_body(char **file)
 {
-    for (size_t i = 0; fileTab[i] ; i++)
-        fileTab[i] = clean_string(fileTab[i]);
-    return fileTab;
+    size_t count = 0;
+
+    for (size_t i = 0; file[i] ; i++) {
+        if (file[i][0] != '.' && file[i][0] != '#')
+            count++;
+    }
+    return count;
+}
+
+static char **get_body(char **file)
+{
+    size_t len = 0;
+    char **cpy = NULL;
+
+    for (size_t i = 0; file[i] ; i++)
+        file[i] = clean_string(file[i]);
+    len = count_lines_body(file);
+    if (!(cpy = malloc(sizeof(char *) * (len + 1))))
+        return NULL;
+    cpy[len] = NULL;
+    for (size_t i = 0, j = 0; file[i] ; i++) {
+        if (file[i][0] != '.' && file[i][0] != '#') {
+            cpy[j] = my_strcpy(file[i]);
+            j++;
+        }
+    }
+    free_array_str(file);
+    return cpy;
 }
 
 bool parse_body(file_t *file, char *filepath)
 {
     char **fileTab = get_file(filepath);
 
-    file->body->body = get_body(fileTab);
+    file->body->bodyArray = get_body(fileTab);
     return true;
 }
