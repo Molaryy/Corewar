9f92c034654dafe4750f4f65011058f077d03a57
Bollore <quentin.bollore@epitech.eu>
2023-05-19 18:09:20 +0200

    feat: remove old free that makes memory error -> (asm/destroy/free_header.c)-

--- asm/destroy/free_header.c
+++ asm/destroy/free_header.c
@@ -14,8 +14,6 @@ extern void free_header(file_t *file, parser_t *parser)
     free_body(file);
     free_array_str(file->origin_file);
     free(file->body);
-    free(file->header->description);
-    free(file->header->name);
     free(file->header);
     free(file);
     free_parser(parser);
