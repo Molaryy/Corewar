494aa27feb6d3236fa33800a1e9da785c0cb4add
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-05-19 11:14:45 +0200

    feat: check if there is only a label -> (asm/parsing/body/add_label_to_link.c)-

--- asm/parsing/body/add_label_to_link.c
+++ asm/parsing/body/add_label_to_link.c
@@ -7,13 +7,27 @@
 
 #include "asm.h"
 
-static bool check_repetition_label(body_t *body)
+static void count_repetitions(char *data, char *label, size_t *count)
 {
-    for (; body->labels; body->labels = body->labels->next) {
-    }
+    if (my_strcmp(data, label))
+        *count += 1;
 }
 
-bool add_labels_to_link(body_t *body)
+static bool check_repetition_label(body_t *body)
+{
+    size_t count = 0;
+    link_t *link = body->labels;
+
+    for (; body->labels; body->labels = body->labels->next) {
+        for (; link ; link = link->next)
+            count_repetitions(body->labels->data, link->data, &count);
+        if (count > 1)
+            return false;
+    }
+    return true;
+}
+
+extern bool add_labels_to_link(body_t *body)
 {
     char **pars = NULL;
 
@@ -23,10 +37,13 @@ bool add_labels_to_link(body_t *body)
         if (pars[0][my_strlen(pars[0]) - 1] != ':')
             continue;
         if (!body->labels)
-            body->labels = create_link(body->labels, pars[0]);
+            body->labels = create_link(body->labels, my_strcpy(pars[0]));
         else
-            add_link_end(&body->labels, pars[0]);
+            add_link_end(&body->labels, my_strcpy(pars[0]));
         free_array_str(pars);
     }
+    if (!check_repetition_label(body))
+        return false;
+    free_links(&(body->labels));
     return true;
 }
