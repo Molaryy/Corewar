4ea3c5c3dfc41cc85d45ab73ce479d4722282593
Bollore <quentin.bollore@epitech.eu>
2023-05-19 17:57:33 +0200

    feat: new define and op.c struct -> (includes/asm.h)-

--- includes/asm.h
+++ includes/asm.h
@@ -36,8 +36,28 @@
     #define DIRECT_VALUE '%'
     #define INDIRECT_VALUE is_digit(params[i][0])
 
+    typedef char    args_type_t;
+    #define T_REG           1
+    #define T_DIR           2
+    #define T_IND           4
+    #define T_LAB           8
+
+    #define COREWAR_EXEC_MAGIC      0xea83f3  
+
+    typedef struct op_s {
+        char *mnemonique;
+        char nbr_args;
+        char type[MAX_ARGS_NUMBER];
+        char code;
+        int nbr_cycles;
+        char *comment;
+    } op_t;
+
+    extern  op_t    op_tab[];
+
     typedef struct header_s {
 
+        int magic;
         char *name;
         char *description;
 
