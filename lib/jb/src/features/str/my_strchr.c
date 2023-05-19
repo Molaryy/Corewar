fc38f0affc8fc13bc20ebd59a45b7c7f8f3a869f
Bollore <quentin.bollore@epitech.eu>
2023-05-19 03:34:53 +0200

    feat: function to cut a word

--- lib/jb/src/features/str/my_strchr.c
+++ lib/jb/src/features/str/my_strchr.c
@@ -5,3 +5,17 @@
 ** my_strchr
 */
 
+#include "jb.h"
+
+extern char* my_strchr(const char* __first_str, char index)
+{
+    while (*(__first_str) != '\0') {
+        if (*(__first_str) == index)
+            return ((char*)__first_str);
+        __first_str++;
+    }
+    if (*(__first_str) == index) {
+        return ((char*)__first_str);
+    }
+    return NULL;
+}
