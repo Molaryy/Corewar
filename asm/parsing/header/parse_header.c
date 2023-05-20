986a7949e04e8c65eca6eb288088f85fe1961ac9
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-05-21 01:57:29 +0200

    feat: check if there is no name or no comment -> (asm/parsing/header/parse_header.c)-

--- asm/parsing/header/parse_header.c
+++ asm/parsing/header/parse_header.c
@@ -14,9 +14,26 @@ extern void clean_array(char **array)
         clean_string(array[i]);
 }
 
+static bool check_header_compenents(char **parser)
+{
+    size_t countHeader = 0;
+
+    if (!parser || !parser[0])
+        return false;
+    if (my_strcmp(parser[0], ".name") || my_strcmp(parser[0], ".comment"))
+        countHeader++;
+    if (countHeader == 0)
+        return false;
+    return true;
+}
+
 extern int check_header(char **parser, file_t *file, size_t status)
 {
     clean_array(parser);
+    if (!parser[1])
+        return FAILURE;
+    if (!check_header_compenents(parser))
+        return FAILURE;
     if (is_name(parser[0]))
         q_strcpy(file->header->name, parser[1]);
     if (is_description(parser[0]))
@@ -30,6 +47,7 @@ extern int parse_header(file_t *file, char *filepath)
 {
     char **parser = NULL;
     size_t status = 0;
+    size_t count = 0;
 
     file->origin_file = get_file(filepath);
     clean_array(file->origin_file);
