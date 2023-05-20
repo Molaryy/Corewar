83016c14cc923d57da81c0f0e870c2d65ba128f6
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-05-21 01:56:51 +0200

    feat: set tp NULL some variables -> (asm/destroy/free_header.c)-

--- asm/destroy/free_header.c
+++ asm/destroy/free_header.c
@@ -13,9 +13,14 @@ extern void free_header(file_t *file, parser_t *parser)
 {
     free_body(file);
     free_array_str(file->origin_file);
-    free(file->body);
-    free(file->header);
-    free(file);
-    free_parser(parser);
-    free(parser);
+    if (file->body)
+        free(file->body);
+    if (file->header)
+        free(file->header);
+    if (file)
+        free(file);
+    if (parser) {
+        free_parser(parser);
+        free(parser);
+    }
 }
