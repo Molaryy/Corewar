d1667912f191315d4cd2d554bfb0424af8b1b337
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-05-22 18:19:53 +0200

    fix: fixed cpy_tab functions -> (lib/jb/src/features/array/cpy_tab.c)-

--- lib/jb/src/features/array/cpy_tab.c
+++ lib/jb/src/features/array/cpy_tab.c
@@ -14,6 +14,8 @@ extern char **cpy_tab(char **tab, size_t start)
 
     if (!cpy)
         return NULL;
+    if (!tab[1])
+        start = 0;
     for (int i = start, j = 0; tab[i] != NULL; i++, j++)
         cpy[j] = my_strcpy(tab[i]);
     cpy[lines] = NULL;
