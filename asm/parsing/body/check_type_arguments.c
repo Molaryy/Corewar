e25c3a08e9738b91fcfa1bff5f0dad649c1ec42c
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-05-23 01:15:20 +0200

    fix: global parsing -> (asm/parsing/body/check_type_arguments.c)-

--- asm/parsing/body/check_type_arguments.c
+++ asm/parsing/body/check_type_arguments.c
@@ -9,36 +9,11 @@
 #include "asm.h"
 #include "parser.h"
 
-static size_t get_type(char c, char *name, size_t pos)
-{
-    size_t valueToReturn = 0;
-    char *instructions[] = {"live", "fork", "zjump", "lfork", NULL};
-
-    switch (c) {
-        case 'r': valueToReturn = 0; break;
-        case '%': valueToReturn = 2; break;
-        case '-': valueToReturn = 2; break;
-        default: valueToReturn = 2; break;
-    }
-    for (size_t i = 0; instructions[i]; i++) {
-        if (my_strcmp(name, instructions[i]))
-            valueToReturn = 1;
-    }
-    if (my_strcmp(name, "ld") && valueToReturn != 0)
-        valueToReturn = 1;
-    if (my_strcmp(name, "lld") && valueToReturn != 0)
-        valueToReturn = 1;
-    if (my_strcmp(name, "sti") && (pos == 2 || pos == 3) && valueToReturn == 0)
-        valueToReturn = 2;
-    return valueToReturn;
-}
-
 static bool check_one_arg(parser_t *pars, size_t type, char *name)
 {
     for (size_t i = 0; i < NB_INSTRIUCTIONS; i++){
         if (!my_strcmp(pars[i].name, name))
             continue;
-        my_printf("type == %d && P1 == %d\n", type, pars[i].p1);
         if (pars[i].p1 == type)
             return true;
     }
@@ -64,14 +39,11 @@ static bool check_three_args(parser_t *pars, size_t type, size_t j, char *name)
         if (!my_strcmp(pars[i].name, name))
             continue;
         if (pars[i].p1 == type && j == 0) {
-            //my_printf("P1 = %d && type = %d\n", pars[i].p1, type);
             return true;
         }
         if (pars[i].p2 == type && j == 1){
-            //my_printf("P2 = %d && type = %d\n", pars[i].p2, type);
             return true;
         }
-        //my_printf("P2 = %d && type = %d\n", pars[i].p2, type);
         if (pars[i].p3 == type && j == 2)
             return true;
     }
@@ -86,14 +58,11 @@ extern bool check_type_arguments(char **line, parser_t *pars, size_t start)
 
     if (!line || !pars)
         return false;
-
     for (size_t i = start + 1, j = 0; line[i] ; i++, j++) {
         nbParams = get_number_params(line[start], pars);
         type = get_type(line[i][0], line[start], i);
-        my_printf("type = %d\n", type);
         switch (nbParams) {
-            case 1: checkArg = check_one_arg(pars, type, line[start]);
-                break;
+            case 1: checkArg = check_one_arg(pars, type, line[start]); break;
             case 2: checkArg = check_two_args(pars, type, j, line[start]);
                 break;
             case 3: checkArg = check_three_args(pars, type, j, line[start]);
