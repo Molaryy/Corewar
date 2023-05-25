72a1f8b993d361f5f1ecce7cdbf4b2cd6945e3c1
Bollore <quentin.bollore@epitech.eu>
2023-05-25 07:44:26 +0200

    feat: fix coding style -> (asm/transcription/parameters/label_find.c)

--- asm/transcription/parameters/label_find.c
+++ asm/transcription/parameters/label_find.c
@@ -7,25 +7,35 @@
 
 #include "asm.h"
 
+extern void if_label_exist(file_t *file, int *k, int i)
+{
+    if (!file->champ[i].paramName) {
+        file->label_find[*k].adresse = file->header->prog_size;
+        file->label_find[*k].name = file->champ[i].label;
+    }
+    file->label_find[*k].name = remove_char(file->label_find[*k].name, ':');
+    *(k++);
+}
+
 extern void label_find(file_t *file)
 {
     int k = 0;
+    size_t i;
 
     file->label_find = malloc(sizeof(label_find_t) * file->nbLabels);
-    
     file->header->prog_size = 0;
-    for (size_t i = 0; i != file->nbLinesBody; i++) {
-        if (file->champ[i].label) {
-            if (!file->champ[i].paramName) {
-                file->label_find[k].adresse = file->header->prog_size;
-                file->label_find[k].name = file->champ[i].label;
-            }
-            file->label_find[k].name = remove_char(file->label_find[k].name, ':');
+
+    for (i = 0; i != file->nbLinesBody; i++) {
+        if (file->champ[i].label && !file->champ[i].paramName) {
+            file->label_find[k].adresse = file->header->prog_size;
+            file->label_find[k].name = file->champ[i].label;
+            file->label_find[k].name =
+                    remove_char(file->label_find[k].name, ':');
             k++;
         }
-        if (file->champ[i].paramName)
-            if (starting_prog_size(file, i))
-                continue;
+        if (file->champ[i].paramName && starting_prog_size(file, i)) {
+            continue;
+        }
     }
 }
 
