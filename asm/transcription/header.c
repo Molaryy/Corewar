67f02e7e77878213ab42add48f0d1112e1b33cd9
Bollore <quentin.bollore@epitech.eu>
2023-05-25 08:01:40 +0200

    feat: remove printf -> (asm/transcription/header.c)

--- asm/transcription/header.c
+++ asm/transcription/header.c
@@ -7,44 +7,72 @@
 
 #include "asm.h"
 
+void int_to_hex(int num, int32_t *hexValue)
+{
+    unsigned char *hex_arr = malloc(sizeof(int));
+    unsigned char *reversed_hex_arr;
+
+    if (hex_arr == NULL)
+        return;
+    for (size_t i = 0; i < sizeof(int); i++)
+        hex_arr[sizeof(int) - 1 - i] = (num >> (8 * i));
+    reversed_hex_arr = malloc(sizeof(int));
+    if (reversed_hex_arr == NULL) {
+        free(hex_arr);
+        return;
+    }
+    for (size_t i = 0; i < sizeof(int); i++)
+        reversed_hex_arr[i] = hex_arr[sizeof(int) - 1 - i];
+    *hexValue = *(int32_t*)reversed_hex_arr;
+    free(hex_arr);
+    free(reversed_hex_arr);
+}
+
+void check_parameter(file_t *file, int i, int j)
+{
+    if (file->champ[i].params[j][0] == 'r') {
+        file->header->prog_size++;
+    } else if (file->champ[i].params[j][0] == '%') {
+        file->header->prog_size += 4;
+    } else {
+        file->header->prog_size += 2;
+    }
+}
+
 void get_size_each_argument(file_t *file, int i, int j)
 {
     if (!file->champ[i].isIndex[j]) {
-        if (file->champ[i].params[j][0] == 'r') {
-            //printf("inst -> [%s] register -> %s\n", file->champ[i].paramName,
-                         //                   file->champ[i].params[j]);
-            file->header->prog_size++;
-        } else if (file->champ[i].params[j][0] == '%') {
-            file->header->prog_size += 4;
-            //printf("inst -> [%s] direct -> %s\n", file->champ[i].paramName,
-                                            //file->champ[i].params[j]);
-        } else {
-            file->header->prog_size += 2;
-            //printf("inst -> [%s] indirect -> %s\n", file->champ[i].paramName,
-                                           // file->champ[i].params[j]);
-        }
+        check_parameter(file, i, j);
     } else {
         file->header->prog_size += 2;
-        //printf("inst -> [%s] index -> %s\n", file->champ[i].paramName,
-                   // file->champ[i].params[j]);
     }
+}
+
+extern bool starting_prog_size(file_t *file, int i)
+{
+    file->header->prog_size++;
+    if (file->champ[i].nbParams > 1)
+        file->header->prog_size++;
+    if (file->champ[i].params == NULL)
+        return true;
+    for (int j = 0; file->champ[i].params[j]; j++) {
+            get_size_each_argument(file, i, j);
+    }
+    return false;
 }
 
 void write_header(file_t *file, char *filename)
 {
+    int32_t hexa_prog_size;
+
     file->header->magic = htobe32(COREWAR_EXEC_MAGIC);
     file->header->prog_size = 0;
-    for (int i = 0; i != file->nbLinesBody; i++) {
-        file->header->prog_size++;
-        if (file->champ[i].nbParams > 1)
-            file->header->prog_size++;
-        if (file->champ[i].params == NULL)
+    for (size_t i = 0; i != file->nbLinesBody; i++) {
+        if (file->champ[i].paramName && starting_prog_size(file, i))
             continue;
-        for (int j = 0; file->champ[i].params[j]; j++) {
-            get_size_each_argument(file, i, j);
-        }
     }
-    my_printf("prog_size -> %d\n", file->header->prog_size);
+    int_to_hex(file->header->prog_size, &hexa_prog_size);
+    file->header->prog_size = htobe32(hexa_prog_size);
     file->fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     write(file->fd, file->header, sizeof(header_t));
 }
