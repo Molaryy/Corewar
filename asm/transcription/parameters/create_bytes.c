0ac18e78d4649196c4cabeec23af6e7ff2a00b11
Bollore <quentin.bollore@epitech.eu>
2023-05-27 03:29:01 +0200

    feat: add label working -> (asm/transcription/parameters/create_bytes.c)

--- asm/transcription/parameters/create_bytes.c
+++ asm/transcription/parameters/create_bytes.c
@@ -7,22 +7,21 @@
 
 #include "asm.h"
 
-extern parambyte_t *create_param_byte(const char* param, file_t *file)
+extern parambyte_t *create_param_byte(const char* param, file_t *file, int i)
 {
     parambyte_t *paramByte = malloc(sizeof(parambyte_t));
     unsigned int value = 0;
-
     if (param[0] == '%') {
         file->offset += 4;
         if (param[1] != ':') {
             value = get_value(param, 1);
             paramByte->bytes = get_direct_bytes(value, paramByte);
-        } else
-            my_printf("offset -> %d\n", file->offset);
+        } else {
+            value = one_label_handling(param, file, value, i);
+            paramByte->bytes = get_direct_bytes(value, paramByte);
+        }
     } else if (param[0] == 'r') {
-        file->offset++;
-        value = get_value(param, 0);
-        paramByte->bytes = get_register_bytes(value, paramByte);
+        register_go(file, paramByte, param, value);
     } else {
         file->offset += 4;
         value = get_value(param, 2);
@@ -31,7 +30,7 @@ extern parambyte_t *create_param_byte(const char* param, file_t *file)
     return paramByte;
 }
 
-extern parambyte_t *create_zjmp_bytes(const char *param, file_t *file)
+extern parambyte_t *create_zjmp_bytes(const char *param, file_t *file, int i)
 {
     parambyte_t *paramByte = malloc(sizeof(parambyte_t));
     unsigned int value = 0;
@@ -42,7 +41,7 @@ extern parambyte_t *create_zjmp_bytes(const char *param, file_t *file)
         if (param[1] != ':')
             value = get_value(param, 1);
         else
-            value = one_label_handling(param, file, value);
+            value = one_label_handling(param, file, value, i);
     else
         value = get_value(param, 2);
     file->offset += 2;
@@ -50,22 +49,21 @@ extern parambyte_t *create_zjmp_bytes(const char *param, file_t *file)
     return paramByte;
 }
 
-extern parambyte_t *create_ldi_bytes(const char *param, file_t *file)
+extern parambyte_t *create_ldi_bytes(const char *param, file_t *file, int i)
 {
     parambyte_t *paramByte = malloc(sizeof(parambyte_t));
     unsigned int value = 0;
-
     if (param[0] == '%') {
         file->offset += 2;
         if (param[1] != ':') {
             value = get_value(param, 1);
             paramByte->bytes = get_indirect_bytes(value, paramByte);
-        } else
+        } else {
+            value = one_label_handling(param, file, value, i);
             paramByte->bytes = get_indirect_bytes(value, paramByte);
+        }
     } else if (param[0] == 'r') {
-        file->offset++;
-        value = get_value(param, 0);
-        paramByte->bytes = get_register_bytes(value, paramByte);
+        register_go(file, paramByte, param, value);
     } else {
         file->offset += 2;
         value = get_value(param, 2);
@@ -74,22 +72,21 @@ extern parambyte_t *create_ldi_bytes(const char *param, file_t *file)
     return paramByte;
 }
 
-extern parambyte_t *create_sti_bytes(const char *param, file_t *file)
+extern parambyte_t *create_sti_bytes(const char *param, file_t *file, int i)
 {
     parambyte_t *paramByte = malloc(sizeof(parambyte_t));
     unsigned int value = 0;
-
     if (param[0] == '%') {
         file->offset += 2;
         if (param[1] != ':') {
             value = get_value(param, 1);
             paramByte->bytes = get_indirect_bytes(value, paramByte);
-        } else
+        } else {
+            value = one_label_handling(param, file, value, i);
             paramByte->bytes = get_indirect_bytes(value, paramByte);
+        }
     } else if (param[0] == 'r') {
-        file->offset ++;
-        value = get_value(param, 0);
-        paramByte->bytes = get_register_bytes(value, paramByte);
+        register_go(file, paramByte, param, value);
     } else {
         file->offset += 2;
         value = get_value(param, 2);
@@ -98,7 +95,7 @@ extern parambyte_t *create_sti_bytes(const char *param, file_t *file)
     return paramByte;
 }
 
-extern parambyte_t *create_fork_bytes(const char *param, file_t *file)
+extern parambyte_t *create_fork_bytes(const char *param, file_t *file, int i)
 {
     parambyte_t *paramByte = malloc(sizeof(parambyte_t));
     unsigned int value = 0;
@@ -106,10 +103,10 @@ extern parambyte_t *create_fork_bytes(const char *param, file_t *file)
     if (paramByte == NULL)
         exit(EXIT_FAILURE);
     if (param[0] == '%')
-        if (param[1] != ':')
+        if (param[1] != ':') {
             value = get_value(param, 1);
-        else
-            value = one_label_handling(param, file, value);
+        } else
+            value = one_label_handling(param, file, value, i);
     else
         value = get_value(param, 2);
     file->offset += 2;
